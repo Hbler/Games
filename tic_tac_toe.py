2314db01ac42dc6fbd42f0ec00fe6629052a1f0b
Hugo Bler <mail.hbler@gmail.com>
2022-01-16 09:07:30 -0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,15 @@
 ### Pierian Data Udemy Course - Milestone 1 Project ###
+
+'''
+Your assignment: Create a Tic Tac Toe game. You are free to use any IDE you like.
+
+Here are the requirements:
+
+    2 players should be able to play the game (both sitting at the same computer)
+    The board should be printed out every time a player makes a move
+    You should be able to accept input of the player position and then place a symbol on the board
+'''
+
 ### Steps
 # create and display a board and a reference for input -ok
 # define win condition -ok
